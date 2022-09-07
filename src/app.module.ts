32db2bf8c2408ded44c1194077306848d7b53553
holasoynaoki <naoki13futsal@gmail.com>
2022-09-07 14:57:16 +0900

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { PrismaModule } from 'nestjs-prisma';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/task.module';
 import { join } from 'path';
@@ -14,6 +15,7 @@ import { join } from 'path';
       autoSchemaFile: join(process.cwd(), '../graphql/schema.gql'), // MEMO: productionではschema自動生成はしない
       typePaths: undefined, // MEMO: productionでは生成済みのschemaを使う
     }),
+    PrismaModule.forRoot({ isGlobal: true }),
     TasksModule,
   ],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // enable shutdown hook
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
