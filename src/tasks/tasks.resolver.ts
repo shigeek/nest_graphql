d4e906c7c19aae211d97e41c81fd28f33c74942b
holasoynaoki <naoki13futsal@gmail.com>
2022-09-12 07:37:47 +0900

    :sparkles: Add CRUD resolver

--- src/tasks/tasks.resolver.ts
+++ src/tasks/tasks.resolver.ts
@@ -15,4 +15,14 @@ export class TasksResolver {
   createTask(@Args('name') name: string) {
     return this.tasksService.create(name);
   }
+
+  @Mutation(() => Task)
+  deleteTask(@Args('id') id: number) {
+    return this.tasksService.delete(id);
+  }
+
+  @Mutation(() => Task)
+  updateTask(@Args('id') id: number, @Args('name') name: string) {
+    return this.tasksService.update(id, name);
+  }
 }
